ae46908511682e3aa8de5daec508fdb6ee3fae14
Beny <58214990+Beny14@users.noreply.github.com>
2021-01-20 13:41:52 +0200

    Add files via upload

---
+++ 3. Form.php
@@ -0,0 +1,26 @@
+<?php
+    if (isset($_POST['submit'])){
+        echo "Your First Name is: " . $_POST["FName"] . "<br/>";
+        echo "Your Last Name is: " . $_POST["LName"] . "<br/>";
+        echo "Your City is: " . $_POST["City"] . "<br/>";
+        echo "Your State is: " . $_POST["State"] . "<br/>";
+        echo "<br/>";
+        echo "Your Message is: " . $_POST["Message"];
+    }
+?>
+<!DOCTYPE html>
+    <html lang="en" lang="en">
+        <head>
+            <title>A Web Page</title>
+        </head>
+        <body>
+            <form action="3. Form.php" method="POST">
+                First Name: <input type="text" name="FName"/>
+                Last Name: <input type="text" name="LName"/>
+                City: <input type="text" name="City"/>
+                State: <input type="text" name="State"/>
+                Message: <textarea name="Message" cols="30" rows="5"></textarea>
+            <input type="submit" name="submit" value="Submit Data"/>
+            </form>
+        </body>
+    </html>
